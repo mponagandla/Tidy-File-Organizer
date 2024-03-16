e2ac9e553ca471bde6710a3133a052499aa0e90a
Manoj R Ponagandla <mponagandla@gmail.com>
2024-03-16 18:18:00 -0500

    Added self.fileMap to store file details

--- file_organizer.py
+++ file_organizer.py
@@ -1,4 +1,3 @@
-import fleep
 import filetype
 import os
 import pathlib
@@ -11,36 +10,42 @@ class FileOrganizer:
     def __init__(self, directories):
         self.directories = directories
         self.iter = None
+        self.fileMap = {}
+
     # TODO: Get all the files list from all the directories
 
     def retrieve_files_directories(self):
-        paths_list = []
+        file_paths_list = []
         for directory in self.directories:
             self.iter = pathlib.Path(directory)
             directories = self.iter.iterdir()
             for dir in directories:
-                paths_list.append(dir)
-                # if not dir.is_dir():
-                #     # type = filetype.guess(dir)
-                #     # print(f"{dir} -{' dir' if dir.is_dir() else ' file'} - {type}")
-                #     with open(dir, "rb") as file:
-                #         info = fleep.get(file.read(128))
-                #         #print(f"{dir} -{' dir' if dir.is_dir() else ' file'} - {info.type}")
-                # else: print(f"{dir} - dir")
+                file_paths_list.append(dir)
+                if not dir.is_dir():
+                    with open(dir, "rb") as file:
+                        info = filetype.guess(dir)
+                        if info is not None:
+                            self.fileMap[dir] = {
+                                "file_name" : os.path.basename(dir),
+                                "file_type" : info.MIME
+                            }
+                        else:
+                            self.fileMap[dir] = 'None'
+                else:
+                    self.fileMap[dir] = 'Directory'
 
-        return paths_list
+        return file_paths_list
 
     # TODO: Loop through each file to determine the file types
     # TODO: Move each file to the respective folder
     # TODO: Documents/Tidy/Month/Week/FileType/name_MMDDYYYY.type
 
-
-
     def cleanup(self):
         files = self.retrieve_files_directories()
-        for file in files:
-            print(file)
+
 
 if __name__ == '__main__':
-    file_organizer = FileOrganizer([DESKTOP_DIR])
+    file_organizer = FileOrganizer([DOWNLOADS_DIR])
     file_organizer.cleanup()
+    for key in file_organizer.fileMap:
+        print(key, " ", file_organizer.fileMap[key])
